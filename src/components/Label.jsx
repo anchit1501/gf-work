33c40f15f64e147f305fc3a69cde54661844ae93
anchit1501 <1501.anchit@gmail.com>
2022-09-21 13:01:49 +1000

    Tooltip Added

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default (props) => {
+    const { showTooltip, children } = props;
     return (
-        <label style={{ height: '100%', color: 'black', display: "flex", alignItems: 'center', justifyContent: 'center', paddingLeft: '10px',paddingRight:'10px', borderRight:'1px solid #c0c0c0' }}>
-            {props.children}
-        </label>
+        <>
+            <label style={{ height: '100%', color: 'black', display: "flex", alignItems: 'center', justifyContent: 'center', paddingLeft: '10px', paddingRight: '10px', borderRight: '1px solid #c0c0c0' }}>
+                {props.children}
+            </label>
+        </>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Label from "./Label";
+import Tooltip from "./Tooltip";
 
 export default (props) => {
 
@@ -11,19 +12,31 @@ export default (props) => {
         }, 1000);
     }, []);
 
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+
     return (
         <nav style={{ backgroundColor: '#eeeeef', height: '40px', display: "flex", justifyContent: 'flex-start' }}>
-            <Label>
-                {dateState.toLocaleString('en-US', {
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    second: 'numeric',
-                    hour12: true,
-                })}
-            </Label>
-            <Label>
-                Settings
-            </Label>
+
+            <Tooltip content={today} direction="right">
+                <Label>
+                    {dateState.toLocaleString('en-US', {
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        second: 'numeric',
+                        hour12: true,
+                    })}
+                </Label>
+            </Tooltip>
+            <Tooltip content="Yee-haw!" direction="right">
+                <Label>
+                    Settings
+                </Label>
+            </Tooltip>
         </nav>
     )
 }
